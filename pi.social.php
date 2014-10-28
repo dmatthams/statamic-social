2850aa66c34050b979d0d26b346838ed84bffd63
David Matthams <dmatthams@gmail.com>
2014-10-28 15:39:48 +0000

    Did a bit of hoovering

--- pi.social.php
+++ pi.social.php
@@ -25,36 +25,36 @@ class Plugin_social extends Plugin {
 
         if ($value == "facebook") {
             if ($showcounter == 'true') { 
-                $facebookLayout = 'button_count'; 
-                $facebookWidth = '90';
+                $layout = 'button_count'; 
+                $width = '90';
             } else {
-                $facebookLayout = 'button'; 
-                $facebookWidth = '55';
+                $layout = 'button'; 
+                $width = '55';
             }
-            $html .= '<iframe src="//www.facebook.com/plugins/like.php?href='.$url.'&amp;width=&amp;layout='. $facebookLayout .'&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:20px; width:'.$facebookWidth.'px" allowTransparency="true"></iframe>';
+            $html .= '<iframe src="//www.facebook.com/plugins/like.php?href='.$url.'&amp;width=&amp;layout='. $layout .'&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:20px; width:'.$width.'px" allowTransparency="true"></iframe>';
         } elseif ($value == "twitter") {
             if ($showcounter == 'true') { 
-                $twitterLayout = ""; 
+                $layout = ""; 
             } else {
-                $twitterLayout = "none"; 
+                $layout = "none"; 
             }
-            $html .= "<a href='https://twitter.com/share' class='twitter-share-button' data-url='".$url."' data-count='".$twitterLayout."'>Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
+            $html .= "<a href='https://twitter.com/share' class='twitter-share-button' data-url='".$url."' data-count='".$layout."'>Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
         } elseif ($value == "linkedin") {
             if ($showcounter == 'true') { 
-                $linkedinLayout = "data-counter='right'"; 
+                $layout = "data-counter='right'"; 
             } else {
-                $linkedinLayout = ""; 
+                $layout = ""; 
             }
-            $html .= '<script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script><script type="IN/Share" data-url="'.$url.'"'. $linkedinLayout .'></script>';
+            $html .= '<script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script><script type="IN/Share" data-url="'.$url.'"'. $layout .'></script>';
         } elseif ($value == "pinterest") {
             $html .= '<a href="//www.pinterest.com/pin/create/button/" data-pin-do="buttonBookmark" data-pin-color="red"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_20.png" /></a><script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>';
         } elseif ($value == "google") {
             if ($showcounter == 'true') { 
-                $googleLayout = ''; 
+                $layout = ''; 
             } else {
-                $googleLayout = 'data-annotation="none"';
+                $layout = 'data-annotation="none"';
             }
-            $html .= '<script src="https://apis.google.com/js/platform.js" async defer></script><div class="g-plusone" data-size="medium" '.$googleLayout.' data-href="'.$url.'"></div>';
+            $html .= '<script src="https://apis.google.com/js/platform.js" async defer></script><div class="g-plusone" data-size="medium" '.$layout.' data-href="'.$url.'"></div>';
         }
     }
     return $html;
